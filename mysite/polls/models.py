3977a5adfe1900637285d41c0762e43660f72d0c
AndreMayerT <andremayerusa@gmail.com>
2023-01-21 20:15:13 -0300

    add tests to model and views

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,9 +9,13 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
+
     def __str__(self) -> str:
         return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
